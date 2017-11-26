8b19a47b753618cbaab70967f6f4e5c7ebc80647
Sean Oberoi <xseano@yandex.com>
2017-11-26 14:08:52 -0800

    mysql: add basic connection to the database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ global.Config   = require('./conf/config.json');
 global.Logger   = require('./library/Logger');
 global.Colors   = require('colors');
 global.readline = require('readline');
+global.mysql    = require('mysql');
 global.self     = this;
 global.rl       = readline.createInterface(
   {
@@ -15,6 +16,27 @@ Logger.print("Loading Dependencies...".green.dim);
 
 // Start Logger
 Logger.start();
+
+// MySQL stuff
+if (Config.Server.isDev === true)
+{
+   var unm = 'root';
+   var pwd = '';
+}
+else
+{
+   var unm = Config.Database.U;
+   var pwd = Config.Database.P;
+}
+
+var connect = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: unm,
+    password: pwd,
+    database: 'aqw'
+  }
+);
 
 // Error handling
 process.on('uncaughtException', (error) => Logger.error(error));
